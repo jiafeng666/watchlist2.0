2f11a037dd8bb33b19b128bed75600362b390dfa
jiafeng666 <80165334+jiafeng666@users.noreply.github.com>
2021-06-13 09:26:23 +0800

    Update __init__.py

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -14,7 +14,7 @@ else:
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', dev')  # 等同于 app.secret_key = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')  # 等同于 app.secret_key = 'dev'
 
 # 在扩展实例化前加载配置
 db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
